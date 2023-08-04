659af35c3fcecdd66e7e69f7ceae3706f586d15f
Luzihan <2648116969@qq.com>
2023-08-04 18:55:27 +0800

    promise

--- promise/promise.js
+++ promise/promise.js
@@ -1,6 +1,15 @@
+const fs = require('fs') // node.js
+
+function myReadFile(fileName, callback) {
+    fs.readFile(`./${fileName}.txt`, 'UTF-8', (err, data) => {
+        if (err == null) {
+            callback(data)
+        }
+    })
+}
 function getPromise(fileName) {
     let p = new Promise((resolve, reject) => {
-        fs.readFile(`./data/${fileName}.txt`, 'UTF-8', (err, data) => {
+        fs.readFile(`./${fileName}.txt`, 'UTF-8', (err, data) => {
             if (err == null) {
                 resolve(data)
             } else {
@@ -8,17 +17,26 @@ function getPromise(fileName) {
             }
         })
     })
-    return p
+    return p // Promise 
 }
+getPromise('test')
+    .then((data) => {
+        console.log(data)
+        return getPromise('test1'); // Promise
+    }).then((data) => {
+        console.log(data);
+        // return getPromise(c)
+    })
+// .then((data) => {
+//     console.log(data);
+// }).catch((err) => {
+//     console.log(err);
+// })
 
-getPromise(a).then((data) => {
-    console.log(data)
-    return getPromise(b);
-}).then((data) => {
-    console.log(data);
-    return getPromise(c)
-}).then((data) => {
-    console.log(data);
-}).catch((err) => {
-    console.log(err);
+// 回调地狱的问题
+myReadFile('test', (data) => {
+    console.log('myReadFile', data)
+    myReadFile('test1', (data) => {
+
+    })
 })
