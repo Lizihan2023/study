af4d2280aa4a40555c766a0033ffdaa1b112a729
Luzihan <2648116969@qq.com>
2023-08-06 12:09:43 +0800

    2023-8-06 12:09

--- promise/Promise_study.js
+++ promise/Promise_study.js
@@ -6,7 +6,11 @@ const REJECTED = 'rejected' // 失败
 
 class MyPromise { // 创建类 创建构造函数和执行器
 	constructor(executor) { // executor: 执行
-		executor(this.resolve, this.reject)  // 在类里面调用方法使用this
+		try { // 捕获错误
+			executor(this.resolve, this.reject)  // 在类里面调用方法使用this
+		} catch (e) {
+			this.reject(e)
+		}
 	}
 	// 构造成功失败的方法  将来会直接调用方法 定义为普通函数this指向window或undefined 所以定义为箭头函数 让函数内部指向为这个类的实例对象
 	// status为Promise状态
@@ -33,7 +37,7 @@ class MyPromise { // 创建类 创建构造函数和执行器
 		// this.successCallback && this.successCallback(this.value)
 		// 当多个then方法时以数组的长度为循环条件 通过shift方法 从头往后 再把值赋予
 		while (this.successCallback.length) {
-			this.successCallback.shift()(this.value)
+			this.successCallback.shift()() //当函数传递为数组时不需要传递值，只需要调用即可
 		}
 	}
 	reject = reason => {
@@ -47,36 +51,128 @@ class MyPromise { // 创建类 创建构造函数和执行器
 		// 判断失败回调是否存在,如果存在就调用
 		// this.failCallback && this.failCallback(this.reason)
 		while (this.failCallback.length) {
-			this.failCallback.shift()(this.reason)
+			this.failCallback.shift()() //当函数传递为数组时不需要传递值，只需要调用即可
 		}
 	}
 	then(successCallback, failCallback) {
+		// 当then传入值为空值时 检测函数是否存在 如果有就调用回调 没有就给他补一个函数 传递下去
+		successCallback = successCallback ? successCallback : value => value
+		failCallback = failCallback ? failCallback : reason => { throw reason }
 		// 只有当返回值为promise对象时，后面的then才可以继续调用
 		let promise2 = new MyPromise((resolve, reject) => { // 当创建实例时 我们可以传入一个执行器
 			// 判断状态
 			if (this.status === FULFILLED) {
-				// 给成功回调函数定义一个变量 使他的值可以返回 当调用成功回调时，值会被传入下一个then方法
-				let x = successCallback(this.value)
-				// 判断x的值是普通值还是promise对象
-				//如果是普通值 直接调用resolve
-				//如果是promise对象 查看promise对象返回的结果
-				// 再根据promise对象返回的结果 决定进行resolve还是reject
-				resolvePromise(x, resolve, reject)
+				// 通过异步代码 拿到promise2的值
+				setTimeout(() => {
+					try {
+						// 给成功回调函数定义一个变量 使他的值可以返回 当调用成功回调时，值会被传入下一个then方法
+						let x = successCallback(this.value)
+						// 判断x的值是普通值还是promise对象
+						//如果是普通值 直接调用resolve
+						//如果是promise对象 查看promise对象返回的结果
+						// 再根据promise对象返回的结果 决定进行resolve还是reject
+						resolvePromise(promise2, x, resolve, reject)
+					} catch (e) {
+						// 如果成功回调发生错误，手动调用下一个reject函数
+						reject(e)
+					}
+				}, 0)
 			} else if (this.status === REJECTED) {
-				failCallback(this.reason)
+				setTimeout(() => {
+					try {
+						// 给成功回调函数定义一个变量 使他的值可以返回 当调用成功回调时，值会被传入下一个then方法
+						let x = failCallback(this.reason)
+						// 判断x的值是普通值还是promise对象
+						//如果是普通值 直接调用resolve
+						//如果是promise对象 查看promise对象返回的结果
+						// 再根据promise对象返回的结果 决定进行resolve还是reject
+						resolvePromise(promise2, x, resolve, reject)
+					} catch (e) {
+						// 如果成功回调发生错误，手动调用下一个reject函数
+						reject(e)
+					}
+				}, 0)
 			} else {
 				// 当为等待状态时 ，把成功和失败的函数存储起来
-				this.successCallback.push(successCallback)
-				this.failCallback.push(failCallback)
+				this.successCallback.push(() => {
+					setTimeout(() => {
+						try {
+							// 给成功回调函数定义一个变量 使他的值可以返回 当调用成功回调时，值会被传入下一个then方法
+							let x = successCallback(this.value)
+							// 判断x的值是普通值还是promise对象
+							//如果是普通值 直接调用resolve
+							//如果是promise对象 查看promise对象返回的结果
+							// 再根据promise对象返回的结果 决定进行resolve还是reject
+							resolvePromise(promise2, x, resolve, reject)
+						} catch (e) {
+							// 如果成功回调发生错误，手动调用下一个reject函数
+							reject(e)
+						}
+					}, 0)
+				})
+				this.failCallback.push(() => {
+					setTimeout(() => {
+						try {
+							// 给成功回调函数定义一个变量 使他的值可以返回 当调用成功回调时，值会被传入下一个then方法
+							let x = failCallback(this.reason)
+							// 判断x的值是普通值还是promise对象
+							//如果是普通值 直接调用resolve
+							//如果是promise对象 查看promise对象返回的结果
+							// 再根据promise对象返回的结果 决定进行resolve还是reject
+							resolvePromise(promise2, x, resolve, reject)
+						} catch (e) {
+							// 如果成功回调发生错误，手动调用下一个reject函数
+							reject(e)
+						}
+					}, 0)
+				})
 				// 当调用多个then方法时，存储一个值已经不满足需求，需要把默认值赋为数组[],再通过push存储起来
 			}
 		})
 		return promise2
 	}
+	static all(array) {
+		let result = []
+		let index = 0
+		return new MyPromise((resolve, reject) => {
+			function addData(key, value) {
+				result[key] = value
+				index++
+				if (index === array.length) {
+					resolve(result)
+				}
+			}
+			for (let i = 0; i < array.length; i++) {
+				let current = result[i]
+				if (current instanceof MyPromise) {
+					// promise对象
+					current.then(value => addData(i, value), reason => reject(reason))
+				} else {
+					// 普通值
+					addData(i, array[i])
+				}
+			}
+			// resolve(result)
+		})
+	}
+	static resolve(value) {
+		if(value instanceof MyPromise) {
+			// promise对象
+			return value
+		} else {
+			// 普通值 创建一个promise对象
+			return new MyPromise(resolve => resolve(value))
+		}
+	}
 }
 // 判断x的值是普通值还是promise对象 只用判断x是不是MyPromise这个对象的实例 如果是实例他就是一个promise对象，如果不是他就是一个普通值
-function resolvePromise(x, resolve, reject) {
-	if(x instanceof MyPromise) {
+function resolvePromise(promise2, x, resolve, reject) {
+	// promise2的返回值就是x， 判断x和promise2是否相等来确定是否为循环调用
+	if (promise2 === x) {
+		// 如果等于了 阻止程序运行下去 返回reject方法
+		return reject(new TypeError('Chaining cycle detected for promise #<Promise>'))
+	}
+	if (x instanceof MyPromise) {
 		// promise对象
 		//x.then(value => resolve(value), reason => reject(reason))
 		x.then(resolve, reject)
@@ -85,6 +181,8 @@ function resolvePromise(x, resolve, reject) {
 		resolve(x)
 	}
 }
+
+
 
 module.exports = MyPromise
 
--- promise/index.js
+++ promise/index.js
@@ -11,33 +11,97 @@
 // 内部调用then方法时 判断此时状态是否为pending 确保状态更改后不会再有改变,
 // 执行时判断当前状态为成功还是失败，调用对应函数方法并赋予对应值 如果有异步执行 当前状态为等待， 应存储成功和失败的回调函数 赋予初始值, 在各个方法内部判断是否存在回调函数 存在就调用 解决异步处理的问题
 // 7.then方法是可以链式调用的，后面的then方法的回调函数拿到的值是上一个then方法的回调函数的返回值
-// 8.链式调用then方法的回调函数中不能够返回当前这个then方法它返回的值 即then()和then(other())的返回值不能一样
+// 8.链式调用then方法的回调函数中不能够返回当前这个then方法它返回的值 即then()和then(other())的返回值不能一样不然就会造成promise的循环报错
 const MyPromise = require('./Promise_study')
 
-let promise = new MyPromise((resolve, reject) => {
-	// setTimeout(() => {
-	//	resolve('成功.....')
-	// }, 2000);
-	resolve('成功.....')
-	// reject('失败')
-})
+// function p1() {
+// 	return new Promise((resolve, reject) => {
+// 		resolve('hello')
+// 	})
+// }
 
-// 把promise用变量来接收,可以调用then方法,传入两个参数,个成功一个失败
-// promise.then(value => {
-// 	console.log(value)
-// }, reason => {
-// 	console.log(reason)
-// })
+// Promise.resolve(10).then(value => console.log(value))
+// Promise.resolve(p1()).then(value => console.log(value))
 
-function other() {
-	return new MyPromise((resolve, reject) => {
-		resolve('other')
+
+function p1() {
+	return new MyPromise((resolve, reject) =>{
+		setTimeout(() => {
+			resolve('p1')
+		}, 2000)
 	})
 }
 
-promise.then(value => {
-	console.log(value)
-	return other()
-}).then(value => {
-	console.log(value)
-})
+function p2() {
+	return new MyPromise((resolve, reject) => {
+		resolve('p2')
+	})
+}
+// promise的resolve方法实现
+// const promise = new MyPromise((resolve, reject) => {
+// 	resolve('成功')
+// })
+// promise.then(value => {
+// 	console.log(value)
+// 	return p1();
+// }).then(value => {
+// 	console.log(value)
+// })
+// MyPromise.resolve(100).then(value => console.log(value))
+// MyPromise.resolve(p1()).then(value => console.log(value))
+
+
+// MyPromise.all(['a', 'b', p1(), p2(), 'c']).then((result) => {
+// 	console.log(result)
+// })
+// let promise = new MyPromise((resolve, reject) => {
+// 	// setTimeout(() => {
+// 	// 	resolve('成功.....')
+// 	// }, 2000);
+// 	// throw(new Error('executor error'))
+// 	 resolve('成功.....')
+// 	// reject('失败')
+// })
+
+// // 把promise用变量来接收,可以调用then方法,传入两个参数,个成功一个失败
+// // promise.then(value => {
+// // 	console.log(value)
+// // }, reason => {
+// // 	console.log(reason)
+// // })
+
+// function other() {
+// 	return new MyPromise((resolve, reject) => {
+// 		resolve('other')
+// 	})
+// }
+
+// // 当then传入为空值 他的状态一直会传递下去 直到有传入值
+// promise.then().then().then(value => console.log(value), reason => {console.log(reason)})
+
+// promise.then(value => {
+// 	console.log(value)
+// 	// throw new Error('then Error')
+// 	return 'aaaa'
+// }, reason => {
+// 	console.log(reason.message)
+// 	return 10000
+// }).then(value => {
+// 	console.log(value)
+// }) // .then(value => {
+// 	console.log(value)
+// }, reason => {
+// 	console.log('aaaaaa')
+// 	console.log(reason.message)
+// })
+
+// let p1 = promise.then(value => {
+// 	console.log(value)
+// 	return p1
+// }) // then方法的返回值为promise对象的返回值 会造成循环
+
+// p1.then(value => {
+// 	console.log(value)
+// }, reason => {
+// 	console.log(reason.message)
+// })
